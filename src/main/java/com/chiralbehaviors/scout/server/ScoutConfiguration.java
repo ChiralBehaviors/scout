ea255c73279117ab61d10e4ea73ef2297f21ac50
Hallie Parry <halloran.parry@gmail.com>
2014-11-13 16:43:37 -0800

    add fromYaml() method to ScoutConfiguration

--- src/main/java/com/chiralbehaviors/scout/server/ScoutConfiguration.java
+++ src/main/java/com/chiralbehaviors/scout/server/ScoutConfiguration.java
@@ -15,6 +15,14 @@
  */
 package com.chiralbehaviors.scout.server;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
 import io.dropwizard.Configuration;
 
 /**
@@ -22,6 +30,14 @@ import io.dropwizard.Configuration;
  *
  */
 public class ScoutConfiguration extends Configuration {
+
+    public static ScoutConfiguration fromYaml(InputStream yaml)
+                                                               throws JsonParseException,
+                                                               JsonMappingException,
+                                                               IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        return mapper.readValue(yaml, ScoutConfiguration.class);
+    }
 
     public String configFile;
 
